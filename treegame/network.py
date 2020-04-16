05e45efe3f981a120dce701827ffcf52361e34a5
Julian Keupp <julian.keupp@rub.de>
2020-04-16 18:53:09 +0200

    small bugfix

--- treegame/network.py
+++ treegame/network.py
@@ -191,7 +191,7 @@ class Client(object):
             elif txt.count(' sold tree' ) != 0:
                 # Needs to be checked if any of that works!
                 pos = tuple([int(x) for x in txt.split('sold tree')[-1].split(' ')[-1].split('_')])
-                tree = main.board.field.get_tile_occupation(numpy.array(pos,dtype='int'))
+                tree = self.main.board.field.get_tile_occupation(numpy.array(pos,dtype='int'))
                 self.main.board.chop_tree(numpy.array(pos,dtype='int'),tree)
                 pass
             elif txt.count(' is done' ) != 0:
